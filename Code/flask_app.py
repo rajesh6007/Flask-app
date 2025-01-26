a59cde065859e288fffadf42aaba5dbb66fb6084
rajesh6007 <hamalr551@gmail.com>
2025-01-26 18:47:10 +0545

    I am commiting

---
+++ Code/flask_app.py
@@ -0,0 +1,40 @@
+import os
+from flask import Flask, request, jsonify, render_template
+import joblib
+import pandas as pd
+import numpy as np
+
+# Load the trained model
+MODEL_PATH = "forecast_model.json"  # Path to your saved model
+model = joblib.load(MODEL_PATH)
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Route to handle predictions
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Parse input data
+        data = request.json
+        input_data = pd.DataFrame(data, index=[0])
+
+        # Make predictions
+        prediction = model.predict(input_data)
+
+        # Return prediction as JSON
+        return jsonify({"prediction": prediction.tolist()})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+# Main entry point
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port=int(os.environ.get('PORT', 5000)))
