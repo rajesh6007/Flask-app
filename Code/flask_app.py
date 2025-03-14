f2579c3636dd6e66a5e1eb37a3338f91d71220a7
rajesh6007 <hamalr551@gmail.com>
2025-03-14 20:20:30 +0545

    sdf

--- Code/flask_app.py
+++ Code/flask_app.py
@@ -164,7 +164,7 @@ def getdata() :
     
 
 if __name__ == '__main__':
-    #app.run(host = '0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    app.run(host = '0.0.0.0', port=int(os.environ.get('PORT', 5000)))
     
-    app.run(host='0.0.0.0', port=3000, debug=True)
+   # app.run(host='0.0.0.0', port=3000, debug=True)
 
